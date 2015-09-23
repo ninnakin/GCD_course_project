858ea3cd791a1eaa1a7588f01f56a1d41dee2980
ninnakin <kristinajuhlin@gmail.com>
2015-09-23 17:25:01 +0200

    Updated run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -6,7 +6,7 @@
 # 5) From the data set in step 4, creates a second, independent tidy data set with the 
 #    average of each variable for each activity and each subject.
 
-
+# I have downloaded and unzipped the data to 
 # Read and merge the data from the train and test folders 
 library(data.table)
 X <- rbind(read.table("UCI HAR Dataset\\test\\X_test.txt"),read.table("UCI HAR Dataset\\train\\X_train.txt"))
@@ -15,18 +15,24 @@ subject <- rbind(read.table("UCI HAR Dataset\\test\\subject_test.txt"),read.tabl
 
 # Read the file containing all column descriptions to identify columns with mean and std values
 # Use regular expressions to identify the columns
-features<-read.table("features.txt")
-features[grepl("mean",features[,2],ignore.case=TRUE) | grepl("std",features[,2],ignore.case=TRUE),]
+features<-read.table("UCI HAR Dataset\\features.txt")
+mean.and.std<-grepl("mean",features[,2],ignore.case=TRUE) | grepl("std",features[,2],ignore.case=TRUE)
 
 # Get the columns from X corresponding to the mean or standard deviation
 # Use the names from features.txt as column names
-X2 <- X[, grepl("mean",features[,2],ignore.case=TRUE) | grepl("std",features[,2],ignore.case=TRUE)]
-names(X2)<-features[grepl("mean",features[,2],ignore.case=TRUE) | grepl("std",features[,2],ignore.case=TRUE),2]
+# Todo: 
+# - replace '-' by '.'
+# - remove '()'
+# - remove columns starting with angle()
+X.std.and.mean <- X[,mean.and.std]
+names(X.std.and.mean)<-features[mean.and.std,2]
 
 # Link the values to activity and subject
 names(subject)="subject"
 names(y)="activity"
-X3 <- cbind.data.frame(subject, y, X2)
+# use merge or join instead?
+# no, nothing to join on (no id in X)
+X3 <- cbind.data.frame(subject, y, X.std.and.mean)
 
 # Create new dataset with average of each variable for each activity and each subject
 # Probably good idea to apply some version of plyr to calculate mean for the columns
